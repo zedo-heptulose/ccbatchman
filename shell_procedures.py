b4524641e6f7d52e9e61289e1ea43a94701674c5
zedo-heptulose <baylissgage@gmail.com>
2024-09-04 23:36:03 -0400

    shell procedures now import orca before running commands

--- shell_procedures.py
+++ shell_procedures.py
@@ -3,15 +3,20 @@ import shutil
 import subprocess
 import re
 
+def import_orca(c_dir):
+    subprocess.run(f'module import gnu openmpi orca',shell=True,cwd=container_dir)
+
 def request_orbitals(jobname, container_dir = False):
     if container_dir == False:
         container_dir = './{jobname}'
+    import_orca(container_dir)
     process1 = subprocess.run(f'orca_2mkl {jobname} -molden',shell=True,cwd=container_dir)
     process2 = subprocess.run(f'orca_plot {jobname}.uno -i',shell=True,cwd=container_dir)
 
 def request_uvvis_spectra(jobname,container_dir = False):
     if container_dir == False:
         container_dir = './{jobname}'
+    import_orca(container_dir)
     process1 = subprocess.run(f'orca_mapspc {jobname}.out ABS -x010000 -x130000 -w1000',shell=True,cwd=container_dir)
 
 def uvvis_whole_dir(directory):
